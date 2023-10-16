d28fa4dcee9aa4369d92f666507b740a4036ebe9
Anders Milje <anders.milje@computas.com>
2023-10-16 09:28:32 +0200

    feat: add storage account to output

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,16 @@
 locals {
   app = length(azurerm_linux_function_app.app) > 0 ? azurerm_linux_function_app.app : azurerm_windows_function_app.app
+  storage_account = module.storageaccount.azurerm_storage_account
 }
 
 output "azurerm_function_app" {
   description = "The Azure Function app resource."
   value       = element(local.app, 0)
+}
+
+output "azurerm_storage_account" {
+  description = "The Azure Storage Account used by the Azure Function app resource."
+  value       = local.storage_account
 }
 
 output "identity" {
