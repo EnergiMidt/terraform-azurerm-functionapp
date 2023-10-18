cc5dc034465224d317a7599eb8933807b3e2aa98
Anders Milje <anders.milje@computas.com>
2023-10-18 11:12:51 +0200

    feat: use existing storage account if exists

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,10 @@
 locals {
   app = length(azurerm_linux_function_app.app) > 0 ? azurerm_linux_function_app.app : azurerm_windows_function_app.app
-  storage_account = length(module.storageaccount) > 0 ? module.storageaccount[0].azurerm_storage_account : null
 }
 
 output "azurerm_function_app" {
   description = "The Azure Function app resource."
   value       = element(local.app, 0)
-}
-
-output "azurerm_storage_account" {
-  description = "The Azure Storage Account used by the Azure Function app resource."
-  value       = local.storage_account
 }
 
 output "identity" {
